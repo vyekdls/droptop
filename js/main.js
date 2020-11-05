d575968e2980624350b9b866f66768885a6ed52f
vyekdls <72438701+vyekdls@users.noreply.github.com>
2020-11-06 00:27:23 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,146 +1,82 @@
+// main.js
 
-
-$(function () {
-    var img_num = 0; // 이미지 번호
-    var duration = 3000; // 인터벌 시간
-    var slider;   // slider 실행 객체 변수
+// 2단계 메뉴
+$(function(){
+    $('.depth1 > li').hover(
+      function(){
+        $('.depth2').stop().slideDown(200);
+      },
+      function(){
+        $('.depth2').stop().slideUp(200);
+      }
+    );
+  })
   
-    // 초기화
-    $('.slider > img').fadeOut();
-    $('.slider > img').eq(img_num).fadeIn();
+  // bxslider 
+  $(document).ready(function(){
   
-    // 
-    function changeSlider() {
-      // 전체 이미지 fadeOut
-      $('.slider > img').fadeOut();
-      // 현재 이미지 fadeIn
-      $('.slider > img').eq(img_num).fadeIn();
-      // pager UI 업데이트
+    $('#visual').imagesLoaded( function() {
+      // images have loaded
+      $('.slider').bxSlider({
+        // 효과: horizontal', 'vertical', 'fade'
+        mode: 'fade',
+        // 인터벌 시간
+        pause: 5000,
+        // 자동재생: true | false
+        auto: true
+      });
+    });
+  
+  });
+  
+  
+  // Pick slider
+  $(function () {
+    var left = 0;
+    var duration = 3000;
+    var slider;
+    
+    function changeSlider(paramLeft) {
       $('.pager > a').removeClass('active');
-      $('.pager > a').eq(img_num).addClass('active');
+      $('.pager > a').eq(paramLeft).addClass('active');
     }
   
-
-    // 슬라이더 재생
     function sliderStart() {
       slider = setInterval(function () {
-        // 마지막 이미지 번호이면 처음으로 
-        if (img_num >= 2) { img_num = 0 } else {
-          // 다음 이미지 번호로
-          img_num = img_num + 1;
+        $("#content > .best").hide();
+        if(left > 1) {
+          left = 0;
         }
-        changeSlider(img_num);
+        else {
+          left = left + 1;
+        }
+        //$('#pick > ul > li').css('left', left);
+        $("#content > .best").eq(left).show();
+        console.log(left);
+        changeSlider(left);
       }, duration);
     }
   
     // 슬라이더 정지
     function sliderStop() {
       clearInterval(slider);
-      console.log('stop');
+      // console.log('stop');
     }
   
     sliderStart();
   
     // 마우스가 올라가면 슬라이더 정지
-    $('.slider, .left, .right').hover(
-      function () {
+    $('.best, .pager').hover(
+      function() {
         sliderStop();
       },
-      function () {
+      function() {
         sliderStart();
-        console.log('start');
+        // console.log('start');
       }
     )
   
-    // 이전 이미지 버튼
-    $('.controls > .left').click(function () {
-      if(img_num > 0) {
-        img_num = img_num - 1
-      } else { img_num = 2; }
-      console.log(img_num);
-      changeSlider();
-    })
+  }) //$end
+
+
   
-    // 다음 이미지 버튼
-    $('.controls > .right').click(function () {
-      if(img_num < 2) {
-        img_num = img_num + 1;
-      } else { img_num = 0; }
-      console.log(img_num);
-      changeSlider();
-    })
-  
-
-    
-    $('.depth1 > li').hover(
-        function() {
-            $('.accent').css('display','block');
-        },
-
-        function() {
-            $('.accent').css('display','none');
-        }
-    )
-
-    $('.depth1 > li:nth-child(1)').hover(
-        function() {
-            $('.accent').css('left','288px');
-        },
-
-        function() {
-            $('.accent').css('display','none');
-        }
-    )
-    
-    $('.depth1 > li:nth-child(2)').hover(
-        function() {
-            $('.accent').css('left','400px');
-        },
-
-        function() {
-            $('.accent').css('display','none');
-        }
-    )
-
-    $('.depth1 > li:nth-child(3)').hover(
-        function() {
-            $('.accent').css('left','512px');
-        },
-
-        function() {
-            $('.accent').css('display','none');
-        }
-    )
-
-    $('.depth1 > li:nth-child(4)').hover(
-        function() {
-            $('.accent').css('left','644px');
-        },
-
-        function() {
-            $('.accent').css('display','none');
-        }
-    )
-    
-    $('.depth1 > li:nth-child(5)').hover(
-        function() {
-            $('.accent').css('left','776px');
-        },
-
-        function() {
-            $('.accent').css('display','none');
-        }
-    )
-
-    $('.depth1 > li:nth-child(6)').hover(
-        function() {
-            $('.accent').css('left','913px');
-        },
-
-        function() {
-            $('.accent').css('display','none');
-        }
-    )
-
-
-  }) // $end
